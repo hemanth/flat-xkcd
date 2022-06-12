7b7a7c227006d8678e60fcc7ae74381407e7980a
hemanth.hm <hemanth.hm@gmail.com>
2022-06-12 10:15:11 -0700

    using flat@0.0.14
    Let see if that fixes the broken action.

--- postprocess.js
+++ postprocess.js
@@ -1,4 +1,4 @@
-import { readJSON, readImageFromURL, writeImage } from 'https://deno.land/x/flat@0.0.10/mod.ts'
+import { readJSON, readImageFromURL, writeImage } from 'https://deno.land/x/flat@0.0.14/mod.ts'
 
 const filename = Deno.args[0]
 const data = await readJSON(filename)
